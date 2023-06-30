b38e1e42eb54b8d499c26af2adccdac9d140f8be
Logan Wang <logan.wang@glowing.com>
2023-06-30 17:07:11 +0800

    add installed tasks

--- Justfile
+++ Justfile
@@ -41,3 +41,18 @@ push-changes:
     )
     git commit -am "$COMMIT_MSG"
     git push
+
+# generate installed casks list json file
+@generate-cask-list: && preview
+    brew list --cask | xargs brew info --cask --json=v2 | \
+    jq '[."casks"|.[]|{token,full_token,name:.name[0],homepage,desc,caveats,installed}]' > installed-casks.json
+
+# install all casks in the list file
+@cask-install-all:
+    jq '.[]|.["token"]' installed-casks.json | xargs brew install --cask
+
+# install casks interactively
+@cask-install:
+    jq -r '.[]|{"token", "installed"} | "\(.token) \(.installed)"' installed-casks.json | \
+    fzf -m --bind 'alt-a:select-all,alt-d:deselect-all' | \
+    cut -d' ' -f1 | xargs brew install --cask
--- scripts/list-diff.py
+++ scripts/list-diff.py
@@ -29,6 +29,36 @@ def diff_message():
         message.append('Updated:\n' + '\n'.join(changed_info))
     return '\n\n'.join(message)
 
+def cask_diff_message():
+    with open('./installed-casks.json') as f:
+        list = json.load(f)
+    casks = dict(map(lambda x: (x['token'], x), list))
+    try:
+        old_content = subprocess.check_output(['git', 'show', 'HEAD:installed-casks.json'], stderr=subprocess.DEVNULL)
+        old_list = json.loads(old_content)
+        old_casks = dict(map(lambda x: (x['token'], x), old_list))
+    except subprocess.CalledProcessError:
+        old_casks = {}
+
+    old_keys = set(old_casks.keys())
+    new_keys = set(casks.keys())
+
+    added = sorted(new_keys - old_keys)
+    removed = sorted(old_keys - new_keys)
+    changed = sorted(o for o in old_keys & new_keys if casks[o]['installed'] != old_casks[o]['installed'])
+    message = []
+    if added:
+        added_info = ['  - {}({})'.format(f, casks[f]['installed']) for f in added]
+        message.append('Added:\n' + '\n'.join(added_info))
+    if removed:
+        removed_info = ['  - {}({})'.format(f, old_casks[f]['installed']) for f in removed]
+        message.append('Removed:\n' + '\n'.join(removed_info))
+    if changed:
+        changed_info = ['  - {}({} -> {})'.format(f, old_casks[f]['installed'], casks[f]['installed']) for f in changed]
+        message.append('Updated:\n' + '\n'.join(changed_info))
+    return 'Casks:\n------\n' + '\n\n'.join(message)
+
 if __name__ == '__main__':
     print(diff_message())
+    print(cask_diff_message())
 
